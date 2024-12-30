e87f435eb5771029852ab36bd959e1d704b9259a
thinh <thinhqn999@gmail.com>
2024-12-30 21:35:34 +0700

    hoàn thiện giao diện đăng ký và đăng nhập

--- RutGonLink/RutGonLink/Program.cs
+++ RutGonLink/RutGonLink/Program.cs
@@ -30,6 +30,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
